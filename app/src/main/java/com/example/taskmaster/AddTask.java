404cde510edcc181d019a45de64dc0b99cde6afa
EsraaAbuHana <esraa.abuhana@gmail.com>
2021-05-16 23:38:46 +0300

    read done

--- app/src/main/java/com/example/taskmaster/AddTask.java
+++ app/src/main/java/com/example/taskmaster/AddTask.java
@@ -2,27 +2,33 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class AddTask extends AppCompatActivity {
-TextView submit;
-EditText add;
+    TextView submit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
-submit=findViewById(R.id.textView4);
-submit.setVisibility(View.INVISIBLE);
-add= findViewById(R.id.editTextTextPersonName);
-add.setHint("MY TASK");
+        submit = findViewById(R.id.textView4);
+        submit.setVisibility(View.INVISIBLE);
+       getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent back = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(back);
+        return true;
     }
 
-
     public void submitTask(View view) {
-submit.setVisibility(View.VISIBLE);
+        submit.setVisibility(View.VISIBLE);
     }
 
 }
