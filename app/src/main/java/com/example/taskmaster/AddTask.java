df4ae3b76f6b0ed5e8ea424d2b44a02faddcde14
EsraaAbuHana <esraa.abuhana@gmail.com>
2021-05-16 18:43:32 +0300

    spaceing

--- app/src/main/java/com/example/taskmaster/AddTask.java
+++ app/src/main/java/com/example/taskmaster/AddTask.java
@@ -4,20 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class AddTask extends AppCompatActivity {
 TextView submit;
+EditText add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
 submit=findViewById(R.id.textView4);
 submit.setVisibility(View.INVISIBLE);
+add= findViewById(R.id.editTextTextPersonName);
+add.setHint("MY TASK");
     }
 
 
     public void submitTask(View view) {
 submit.setVisibility(View.VISIBLE);
     }
+
 }
