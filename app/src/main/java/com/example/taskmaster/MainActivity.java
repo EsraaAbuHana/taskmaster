c7dddec592490efa4adf56d03a3a3215b90de37c
EsraaAbuHana <esraa.abuhana@gmail.com>
2021-05-17 16:55:09 +0300

    initial

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -6,15 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 Button addTask,allTasks;
+Button task1,task2,task3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addTask=findViewById(R.id.button2);
         allTasks=findViewById(R.id.button);
+        task1=findViewById(R.id.button4);
+        task2=findViewById(R.id.button5);
+        task3=findViewById(R.id.button6);
+//        display “{username}’s tasks” above the three task buttons.
+        TextView user=findViewById(R.id.textView8)
+
     }
 
     public void addTask(View view) {
