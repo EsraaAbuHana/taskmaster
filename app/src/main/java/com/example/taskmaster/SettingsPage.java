dc8be41d60b790e996766b2318ba0ece1d67b769
EsraaAbuHana <esraa.abuhana@gmail.com>
2021-05-17 19:48:57 +0300

    done activities

--- app/src/main/java/com/example/taskmaster/SettingsPage.java
+++ app/src/main/java/com/example/taskmaster/SettingsPage.java
@@ -2,13 +2,39 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SettingsPage extends AppCompatActivity {
 
+    String user_name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_page);
+
+        Button submit =findViewById(R.id.button7);
+//
+//        SharedPreferences sharedPreferences= PreferenceManager.getDefaultSharedPreferences(this);
+//        SharedPreferences.Editor passed_name=sharedPreferences.edit();
+//        //       user_name=sharedPreferences.getString();
+        submit.setOnClickListener((view)->{
+            EditText user_name_field=findViewById(R.id.editTextTextPersonName3);
+            String name =user_name_field.getText().toString();
+            SharedPreferences sharedPreferences= PreferenceManager.getDefaultSharedPreferences(this);
+            SharedPreferences.Editor passed_name=sharedPreferences.edit();
+                        passed_name.putString("username",name);
+passed_name.apply();
+            Intent main=new Intent(SettingsPage.this,MainActivity.class);
+            startActivity(main);
+            //       user_name=sharedPreferences.getString();
+
+
+
+        });
     }
 }
--- app/src/main/java/com/example/taskmaster/TaskDetailPage.java
+++ app/src/main/java/com/example/taskmaster/TaskDetailPage.java
@@ -2,7 +2,10 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.TextView;
 
 public class TaskDetailPage extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class TaskDetailPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_detail_page);
+
+        TextView task_heading = findViewById(R.id.textView6);
+        TextView body = findViewById(R.id.textView7);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String title = getIntent().getStringExtra("title");
+        task_heading.setText(sharedPreferences.getString("username", "User") + "'s " + title + " Detail");
+
+        body.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+
+    if(title==null){
+        task_heading.setText(sharedPreferences.getString("username", "User") + "'s " + " Detail");
+        body.setText("No Details");
+    }
     }
 }
