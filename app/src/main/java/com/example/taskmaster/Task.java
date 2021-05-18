83bb895c534d31367a1ce45b64e2e086caaedfdb
EsraaAbuHana <esraa.abuhana@gmail.com>
2021-05-18 21:53:53 +0300

    switch

---
+++ app/src/main/java/com/example/taskmaster/Task.java
@@ -0,0 +1,27 @@
+package com.example.taskmaster;
+
+public class Task {
+    private String title;
+    private String body;
+
+    public Task(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
